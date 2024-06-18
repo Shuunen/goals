7a892f236e33700b4b0ceccaffe8755fdb8e755a
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-06-18 21:48:24 +0200

    bump lock file

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -27,10 +27,10 @@ module.exports = {
       from: {
         orphan: true,
         pathNot: [
-          '(^|/)\\.[^/]+\\.(js|cjs|mjs|ts|json)$', // dot files
-          '\\.d\\.ts$',                            // TypeScript declaration files
-          '(^|/)tsconfig\\.json$',                 // TypeScript config
-          '(^|/)(babel|webpack)\\.config\\.(js|cjs|mjs|ts|json)$', // other configs
+          String.raw`(^|/)\.[^/]+\.(js|cjs|mjs|ts|json)$`, // dot files
+          String.raw`\.d\.ts$`,                            // TypeScript declaration files
+          String.raw`(^|/)tsconfig\.json$`,                 // TypeScript config
+          String.raw`(^|/)(babel|webpack)\.config\.(js|cjs|mjs|ts|json)$`, // other configs
         ],
       },
       name: 'no-orphans',
@@ -156,7 +156,7 @@ module.exports = {
       name: 'not-to-spec',
       severity: 'error',
       to: {
-        path: '\\.(spec|test)\\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\\.md)$',
+        path: String.raw`\.(spec|test)\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\.md)$`,
       },
     },
     {
@@ -168,7 +168,7 @@ module.exports = {
         'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
       from: {
         path: '^(src)',
-        pathNot: '\\.(spec|test)\\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\\.md)$',
+        pathNot: String.raw`\.(spec|test)\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\.md)$`,
       },
       name: 'not-to-dev-dep',
       severity: 'error',
